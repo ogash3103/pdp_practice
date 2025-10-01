5d48cd6e7e34e8856d64824743c9f939fa2dac11
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-01 18:44:53 +0500

    learn somethings

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,19 +1,34 @@
 void main() {
-  int a = 5;
-  int b = 9;
-  int c = 7;
+  int a = 10;
+  int b = 5;
+  int c = 8;
+  int d = 3;
 
-  int maxNum = 0;
-  if (a > b && a > c) {
-    maxNum = a;
-    print(maxNum);
-  } else if (b > a && b > c) {
-    maxNum = b;
-    print(maxNum);
-  } else if (c > a && c > b) {
-    maxNum = c;
-    print(maxNum);
+  if (a == b && b == c && c == d) {
+    print("Yig'indisi: a + b + c + d = ${a + b + c + d}");
   } else {
-    print('Nimadir xato');
+    int minNum = a;
+    int maxNum = a;
+
+    if (maxNum < b) {
+      maxNum = b;
+    }
+    if (maxNum < c) {
+      maxNum = c;
+    }
+    if (maxNum < d) {
+      maxNum = d;
+    }
+
+    if (b < minNum) {
+      minNum = b;
+    }
+    if (c < minNum) {
+      minNum = c;
+    }
+    if (d < minNum) {
+      minNum = d;
+    }
+    print("Kattadan kichigi ayirma: ${maxNum - minNum}");
   }
 }
