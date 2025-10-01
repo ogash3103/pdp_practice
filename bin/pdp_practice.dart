d47f78caa183ccae7a6c245ffe8c3c1c3eb4109c
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-01 12:54:51 +0500

    add something

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,4 +1,19 @@
 void main() {
-  int score = 0;
-  
+  int a = 5;
+  int b = 9;
+  int c = 7;
+
+  int maxNum = 0;
+  if (a > b && a > c) {
+    maxNum = a;
+    print(maxNum);
+  } else if (b > a && b > c) {
+    maxNum = b;
+    print(maxNum);
+  } else if (c > a && c > b) {
+    maxNum = c;
+    print(maxNum);
+  } else {
+    print('Nimadir xato');
+  }
 }
