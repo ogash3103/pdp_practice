1b5339d3af9284209bf5571a4ee9a4f4801cdcf2
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-02 10:37:44 +0500

    today start all Loop practice update all code there

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,15 +1,9 @@
 void main() {
-  int score = 55;
-
-  if (score >= 0 && score <= 54) {
-    print('2 - baho');
-  } else if (score >= 55 && score <= 70) {
-    print('3 - baho');
-  } else if (score >= 71 && score <= 84) {
-    print('4 - baho');
-  } else if (score>= 85 && score <= 100) {
-    print('5 - baho');
-  } else {
-    print('Faqat 0 - 100 ball gacha');
+  int n = 1;
+  int s = 0;
+  while (n <= 25) {
+    s += n;
+    n++;
   }
+  print(s);
 }
