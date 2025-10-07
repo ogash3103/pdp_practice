d1ad0fe52bd553d2aa5b1c36cd072eb9e8889c28
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-07 22:10:56 +0500

    do while practice

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,14 +1,10 @@
 void main() {
-  int n = 1234;
-  int total = 0;
-  int temp = n;
-  while (temp > 0) {
-    print('n: $temp oxirgi raqam ${temp % 10}');
-    total += temp % 10;
+  int n = 1;
+  int sum = 0;
 
-    // updation
-    temp ~/= 10;
-  }
-
-  print('$n sonining yigindisi $total');
+  do {
+    sum += n;
+    n++;
+  } while (n <= 25);
+  print('N: $sum');
 }
