0afde1905bd0cb3dcf0d613f7410785f8586d35d
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-08 17:45:09 +0500

    for loop practice

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,13 +1,9 @@
 void main() {
-  int n = 1234;
-  int temp = n;
-  int total = 0;
+  int n = 25;
+  int sum = 0;
 
-  do {
-    print('n: $temp oxirgi raqam ${temp % 10}');
-    total += temp % 10;
-    // updation
-    temp ~/= 10;
-  } while (temp > 0);
-  print('natija: $n');
+  for (int i = 0; i <= 25; i++) {
+    sum += i;
+  }
+  print(sum);
 }
