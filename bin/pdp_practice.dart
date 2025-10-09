a946c65a202341a57d3ded92113294d9061cec90
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-09 18:48:44 +0500

    while va for loop da

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,50 +1,17 @@
-String numberToUzbek(int n) {
-  if (n < 1 || n > 99) {
-    throw ArgumentError("Faqat 1…99 oralig'ida son kiriting.");
-  }
-
-  const ones = [
-    "", 
-    "bir", 
-    "ikki", 
-    "uch", 
-    "to'rt", 
-    "besh", 
-    "olti", 
-    "yetti", 
-    "sakkiz", 
-    "to'qqiz", 
-  ];
-
-  const tens = [
-    "", 
-    "o'n", 
-    "yigirma", 
-    "o'ttiz", 
-    "qirq", 
-    "ellik", 
-    "oltmish", 
-    "yetmish", 
-    "sakson", 
-    "to'qson", 
-  ];
-
-  if (n < 10) return ones[n];
-
-  if (n < 20) {
-    final q = n - 10; 
-    if (q == 0) return tens[1]; 
-    return "${tens[1]} ${ones[q]}"; 
-  }
-
-  final onlik = n ~/ 10; 
-  final qoldiq = n % 10; 
-  if (qoldiq == 0) return tens[onlik];
-  return "${tens[onlik]} ${ones[qoldiq]}";
-}
-
 void main() {
-  final n = 12;
+  int a = 35;
+  int sum = 0;
+  int count = 0;
+  int result = 0;
 
-  print(numberToUzbek(n));
+  for (int i = 0; i <= a; i++) {
+    if (i % 5 == 0) sum += i;
+  }
+  print(sum);
+
+  while (count <= 35) {
+    if (count % 5 == 0) result += count;
+    count++;
+  }
+  print(result);
 }
