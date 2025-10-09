685dcc9ef2d8af482dc144ff02c92b6e35c6cf31
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-09 18:32:41 +0500

    1 dan 99 gacha bo’lgan sonlarni son ko’rinishida emas balki yozuv ko’rinishida chiqaradigan dastur

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,10 +1,50 @@
-void main() {
-  int f = 5;
-  int sum = 1;
-
-  for (int i = 1; i <= f; i++) {
-    sum *= i;
+String numberToUzbek(int n) {
+  if (n < 1 || n > 99) {
+    throw ArgumentError("Faqat 1…99 oralig'ida son kiriting.");
   }
 
-  print(sum);
+  const ones = [
+    "", 
+    "bir", 
+    "ikki", 
+    "uch", 
+    "to'rt", 
+    "besh", 
+    "olti", 
+    "yetti", 
+    "sakkiz", 
+    "to'qqiz", 
+  ];
+
+  const tens = [
+    "", 
+    "o'n", 
+    "yigirma", 
+    "o'ttiz", 
+    "qirq", 
+    "ellik", 
+    "oltmish", 
+    "yetmish", 
+    "sakson", 
+    "to'qson", 
+  ];
+
+  if (n < 10) return ones[n];
+
+  if (n < 20) {
+    final q = n - 10; 
+    if (q == 0) return tens[1]; 
+    return "${tens[1]} ${ones[q]}"; 
+  }
+
+  final onlik = n ~/ 10; 
+  final qoldiq = n % 10; 
+  if (qoldiq == 0) return tens[onlik];
+  return "${tens[onlik]} ${ones[qoldiq]}";
+}
+
+void main() {
+  final n = 12;
+
+  print(numberToUzbek(n));
 }
