4502fc9032d61ac43890981fe6c627ffa9825e92
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-09 17:59:33 +0500

    task 1 for loop

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,9 +1,22 @@
 void main() {
   int n = 25;
+
+  for (int i = 1; i <= n; i++) {
+    if (i == 13) {
+      continue;
+    }
+    print(i);
+  }
+  print('=============');
+  int b = 5;
   int sum = 0;
 
-  for (int i = 0; i <= n; i++) {
-    sum += i;
+  for (int i = 1; i <= b; i++) {
+    if (i % 2 == 0) {
+      sum += i * i;
+    } else {
+      print('bolinmaydigan sonlar $i');
+    }
   }
-  print(sum);
+  print('Natija: bolinadigan sonlarni kivadrat yigindisi $sum');
 }
