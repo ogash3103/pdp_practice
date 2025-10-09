5d85018382b1e0a0997c45a4cc197161239e4bd5
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-09 18:09:25 +0500

    Faktorial

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,22 +1,10 @@
 void main() {
-  int n = 25;
+  int f = 5;
+  int sum = 1;
 
-  for (int i = 1; i <= n; i++) {
-    if (i == 13) {
-      continue;
-    }
-    print(i);
+  for (int i = 1; i <= f; i++) {
+    sum *= i;
   }
-  print('=============');
-  int b = 5;
-  int sum = 0;
 
-  for (int i = 1; i <= b; i++) {
-    if (i % 2 == 0) {
-      sum += i * i;
-    } else {
-      print('bolinmaydigan sonlar $i');
-    }
-  }
-  print('Natija: bolinadigan sonlarni kivadrat yigindisi $sum');
+  print(sum);
 }
