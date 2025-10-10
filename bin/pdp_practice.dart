b385e257203277b51ea92264027cce96a6a859d9
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-10 18:27:28 +0500

    2 List list = [65, 31, 87, 5, 99, 47, 79, 105, 34, 45]; //va n soni berilgan. //Shu n soni listda bor yoki yo’qligini aniqlang. //Bor bo’lsa true aks holda false qiymatini consolega chiqaring. //For-in loopdan foydalaning va sikllar sonini //kamaytirish uchun break kalitso’zini ishlating.

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,7 +1,7 @@
 void main() {
   //1. List elementlarining o’rtacha qiymatini topuvchi dastur yuzing
   //for-in loop orqali.
-  List numbers = [6, 25, 91, 23, 72, 9, 18, 6];
+  List numbers = [6, 25, 91, 23, 72, 9, 18, 34];
   int sum = 0;
 
   for (int nums in numbers) {
@@ -11,4 +11,24 @@ void main() {
   int middleNumber = sum ~/ numbers.length;
 
   print(middleNumber);
+
+// 2 List list = [65, 31, 87, 5, 99, 47, 79, 105, 34, 45];
+//va n soni berilgan.
+//Shu n soni listda bor yoki yo’qligini aniqlang.
+//Bor bo’lsa true aks holda false qiymatini consolega chiqaring.
+//For-in loopdan foydalaning va sikllar sonini
+//kamaytirish uchun break kalitso’zini ishlating.
+
+  List list = [65, 31, 87, 5, 99, 47, 79, 105, 34, 45];
+  int n = 31;
+  bool found = false;
+
+  for (var e in list) {
+    if (e == n) {
+      found = true;
+      break;
+    }
+  }
+
+  print(found);
 }
