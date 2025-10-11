ad52cb899314ecd19fc97e9952e19f314571f236
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-11 12:01:18 +0500

    /c) n butun soni berilgan (n > 0).   //Quyidagi yig'indini hisoblovchi programma tuzilsin.   // S = 1.1 - 1.2 + 1.3 - ... ; (n ta go'shiluvchi,   //ishoralar almashib keladi. Shart operatoridan foydalanmang)

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 void main() {
   //a) n soni berilgan. Quyidagi yig'indini
   //hisoblovchi dastur yozing. (1 + 1/2 + 1/3 + 1/4 ..... 1/n ).
@@ -20,4 +22,15 @@ void main() {
   }
 
   print(S);
+
+  //c) n butun soni berilgan (n > 0).
+  //Quyidagi yig'indini hisoblovchi programma tuzilsin.
+  // S = 1.1 - 1.2 + 1.3 - ... ; (n ta go'shiluvchi,
+  //ishoralar almashib keladi. Shart operatoridan foydalanmang)
+
+  for (int i = 1; i <= n; i++) {
+    double sign = pow(-1, i + 1).toDouble();
+    sum += sign * (1 + i / 10);
+  }
+  print('Yig‘indi: ${sum.toStringAsFixed(4)}');
 }
