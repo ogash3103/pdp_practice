855360733bae453510b482c43a717703b7fae738
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-11 12:29:55 +0500

    //b) Lokatr dunyoning bir tomoniga qaratilgan(“s”-shimol,   //“j”-janub, “q”-sharq, "g"-g'arb)   //va uchta raqamli kamanda: 0-o'ngga buril,   //1 chapga buril, 2-burilish 180°.   //C - lakatring boshlang’ich holati va   //K1, K2 - kamandalar berilgan.   //Berilgan kamanda bajarilgandan   //keying lakatr holatini aniqlovchi dastur tuzilsin.

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -16,4 +16,46 @@ void main() {
       print(i);
     }
   }
+
+  print('task b');
+
+  //b) Lokatr dunyoning bir tomoniga qaratilgan(“s”-shimol, 
+  //“j”-janub, “q”-sharq, "g"-g'arb) 
+  //va uchta raqamli kamanda: 0-o'ngga buril, 
+  //1 chapga buril, 2-burilish 180°. 
+  //C - lakatring boshlang’ich holati va 
+  //K1, K2 - kamandalar berilgan. 
+  //Berilgan kamanda bajarilgandan 
+  //keying lakatr holatini aniqlovchi dastur tuzilsin.
+
+  
+  String C = 'q'; 
+  int K = 2;      
+
+  String yangiTomon = ''; 
+
+  if (C == 's') {
+    if (K == 0) {yangiTomon = 'q';}
+    else if (K == 1) {yangiTomon = 'g';}
+    else if (K == 2) {yangiTomon = 'j';}
+  } else if (C == 'j') {
+    if (K == 0) {yangiTomon = 'g';}
+    else if (K == 1) {yangiTomon = 'q';}
+    else if (K == 2) {yangiTomon = 's';}
+  } else if (C == 'q') {
+    if (K == 0) {yangiTomon = 'j';}
+    else if (K == 1) {yangiTomon = 's';}
+    else if (K == 2) {yangiTomon = 'g';}
+  } else if (C == 'g') {
+    if (K == 0) {yangiTomon = 's';}
+    else if (K == 1) {yangiTomon = 'j';}
+    else if (K == 2) {yangiTomon = 'q';}
+  }
+
+  print('Boshlang‘ich yo‘nalish: $C');
+  print('Kamanda: $K');
+  print('Yangi yo‘nalish: $yangiTomon');
+
+
+
 }
