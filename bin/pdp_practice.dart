5b54f3e0fb6d5682e18ca2bd7790a0c1d4b21959
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-11 11:48:02 +0500

    // b) n butun soni berilgan (n > 0).   //Quyidagi ko'paytmani hisoblovchi dastur tuzilsin.   //S = 1.1 * 1.2 * 1.3 * … (n ta ko’pavuvchi)

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -8,4 +8,16 @@ void main() {
     sum += 1 / i;
   }
   print(sum);
+  print('=============');
+  // b) n butun soni berilgan (n > 0).
+  //Quyidagi ko'paytmani hisoblovchi dastur tuzilsin.
+  //S = 1.1 * 1.2 * 1.3 * … (n ta ko’pavuvchi)
+
+  double S = 1;
+
+  for (int i = 1; i <= n; i++) {
+    S *= (1 + i / 10);
+  }
+
+  print(S);
 }
