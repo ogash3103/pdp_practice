de51c3a7a4c58c5481d601bd204d6d92fd27b3a8
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-11 12:38:56 +0500

    //c) Sanani bildiruvchi ikkita butun son berilgan D (kun) va M (ov). //(Kabisa bo'lmagan yil sanasi kiritiladi). //Berilgan sanani ifodalovchi dastur tuzilsin. Kabisa yilida 366 kun, // kabisa bo'lmagan yilda 365 kun bor bo'ladi.

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,3 +1,5 @@
+
+
 void main() {
 // a) N natural soni berilgan.
 //1 dan N gacha bo’lgan tub sonlarni ekranga chiqaruvchi dastur tuzing.
@@ -19,43 +21,83 @@ void main() {
 
   print('task b');
 
-  //b) Lokatr dunyoning bir tomoniga qaratilgan(“s”-shimol, 
-  //“j”-janub, “q”-sharq, "g"-g'arb) 
-  //va uchta raqamli kamanda: 0-o'ngga buril, 
-  //1 chapga buril, 2-burilish 180°. 
-  //C - lakatring boshlang’ich holati va 
-  //K1, K2 - kamandalar berilgan. 
-  //Berilgan kamanda bajarilgandan 
+  //b) Lokatr dunyoning bir tomoniga qaratilgan(“s”-shimol,
+  //“j”-janub, “q”-sharq, "g"-g'arb)
+  //va uchta raqamli kamanda: 0-o'ngga buril,
+  //1 chapga buril, 2-burilish 180°.
+  //C - lakatring boshlang’ich holati va
+  //K1, K2 - kamandalar berilgan.
+  //Berilgan kamanda bajarilgandan
   //keying lakatr holatini aniqlovchi dastur tuzilsin.
 
-  
-  String C = 'q'; 
-  int K = 2;      
+  String C = 'q';
+  int K = 2;
 
-  String yangiTomon = ''; 
+  String yangiTomon = '';
 
   if (C == 's') {
-    if (K == 0) {yangiTomon = 'q';}
-    else if (K == 1) {yangiTomon = 'g';}
-    else if (K == 2) {yangiTomon = 'j';}
+    if (K == 0) {
+      yangiTomon = 'q';
+    } else if (K == 1) {
+      yangiTomon = 'g';
+    } else if (K == 2) {
+      yangiTomon = 'j';
+    }
   } else if (C == 'j') {
-    if (K == 0) {yangiTomon = 'g';}
-    else if (K == 1) {yangiTomon = 'q';}
-    else if (K == 2) {yangiTomon = 's';}
+    if (K == 0) {
+      yangiTomon = 'g';
+    } else if (K == 1) {
+      yangiTomon = 'q';
+    } else if (K == 2) {
+      yangiTomon = 's';
+    }
   } else if (C == 'q') {
-    if (K == 0) {yangiTomon = 'j';}
-    else if (K == 1) {yangiTomon = 's';}
-    else if (K == 2) {yangiTomon = 'g';}
+    if (K == 0) {
+      yangiTomon = 'j';
+    } else if (K == 1) {
+      yangiTomon = 's';
+    } else if (K == 2) {
+      yangiTomon = 'g';
+    }
   } else if (C == 'g') {
-    if (K == 0) {yangiTomon = 's';}
-    else if (K == 1) {yangiTomon = 'j';}
-    else if (K == 2) {yangiTomon = 'q';}
+    if (K == 0) {
+      yangiTomon = 's';
+    } else if (K == 1) {
+      yangiTomon = 'j';
+    } else if (K == 2) {
+      yangiTomon = 'q';
+    }
   }
 
   print('Boshlang‘ich yo‘nalish: $C');
   print('Kamanda: $K');
   print('Yangi yo‘nalish: $yangiTomon');
 
+//c) Sanani bildiruvchi ikkita butun son berilgan D (kun) va M (ov).
+//(Kabisa bo'lmagan yil sanasi kiritiladi).
+//Berilgan sanani ifodalovchi dastur tuzilsin. Kabisa yilida 366 kun,
+// kabisa bo'lmagan yilda 365 kun bor bo'ladi.
 
+  int D = 23;
+  int M = 2;
 
+  int kunlar = 0;
+
+  if (M == 1 || M == 3 || M == 5 || M == 7 || M == 8 || M == 10 || M == 12) {
+    kunlar = 31;
+  } else if (M == 4 || M == 6 || M == 9 || M == 11) {
+    kunlar = 30;
+  } else if (M == 2) {
+    kunlar = 28;
+  }
+  else{
+    print("Bunday oy mavjud emas!");
+    return;
+  }
+
+  if (D >= 1 && D <= kunlar) {
+    print("Sana to‘g‘ri: $D - $M oy");
+  } else {
+    print("Xato sana: $D - $M oyda mavjud emas!");
+  }
 }
