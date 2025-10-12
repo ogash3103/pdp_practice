5c009dae3e2760ad65d15fe45377bd1e07cac2d0
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-12 13:02:34 +0500

    Berilgan sonni teskarisini chiqaruvchi return type li funksiya

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,6 +1,9 @@
 void main() {
   print(tub(5));
+
+  print(TeskariSon(1234));
 }
+
 // tub son topish method
 bool tub(int son) {
   bool tubSon = true;
@@ -19,4 +22,13 @@ bool tub(int son) {
 
 // Berilgan sonni teskarisini chiqaruvchi return type li funksiya
 
+int TeskariSon(int num) {
+  int rev = 0;
+  while (num > 0) {
+    int qoldiq = num % 10;
+    rev = rev * 10 + qoldiq;
+    num = num ~/ 10;
+  }
 
+  return rev;
+}
