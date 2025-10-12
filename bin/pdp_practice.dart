570c8e38709e140f711c659351dc5b4aed096868
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-12 14:57:01 +0500

    Haroratni Farengeytda darajada berganda, uni Selsiy darajaga o’giradigan funksiya

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,22 +1,9 @@
 void main() {
-  print(checkPerfectNumber(28));
+  print(fahrenheitToCelsius(68));
 }
 
-bool checkPerfectNumber(int n) {
-  if (n <= 1) return false;
+double fahrenheitToCelsius(double farengeyt) {
+  double c = (farengeyt - 32) * 5 / 9;
 
-  int sum = 1;
-
-  for (int i = 2; i * i <= n; i++) {
-    if (n % i == 0) {
-      int pair = n ~/ i;
-      if (i != n) sum += i;
-
-      if (pair != n && pair != i) {
-        sum += pair;
-      }
-    }
-  }
-
-  return sum == n;
+  return c;
 }
