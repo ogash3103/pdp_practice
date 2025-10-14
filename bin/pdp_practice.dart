58915e1d98843fd150334ca361b736f825461590
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-14 22:41:01 +0500

    Romb (Diamond)

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -2,9 +2,13 @@ void main() {
   String starWithSpace = '* ';
   String space = " ";
   int column = 6;
-
+  for (int i = 0; i < column; i++) {
+    String row = space * (column - i) + starWithSpace * i;
+    print(row);
+  }
   for (int i = 0; i < column; i++) {
     String row = space * i + starWithSpace * (column - i);
     print(row);
   }
+
 }
