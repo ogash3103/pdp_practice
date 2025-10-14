70c8f2e1d1d557cd0a2877654e6210736e40698a
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-15 00:14:48 +0500

    codeUnitAt

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,14 +1,8 @@
 void main() {
-  String starWithSpace = '* ';
-  String space = " ";
-  int column = 6;
-  for (int i = 0; i < column; i++) {
-    String row = space * (column - i) + starWithSpace * i;
-    print(row);
-  }
-  for (int i = 0; i < column; i++) {
-    String row = space * i + starWithSpace * (column - i);
-    print(row);
+  String simFamilya = "O'g'abek Faxriddinov";
+
+  for (int i = 0; i < simFamilya.length; i++) {
+    print("$i - index ${simFamilya[i]} ${simFamilya.codeUnitAt(i)}");
   }
 
 }
