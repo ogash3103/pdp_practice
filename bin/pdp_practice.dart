23219ceea30e9eab6d9b25fb09cd2a3b43ea753d
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-15 13:12:29 +0500

    Berilgan indexdan oldingi belgini Unicode ini console ga chiqaring. Namuna: str = "Dasturlash" index = 1 -> D -> 68 index = 4 -> t -> 116

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,8 +1,21 @@
 void main() {
   String str = "Dastrulash";
+  int index = 3;
 
   for (int i = 0; i < str.length; i++) {
     print("$i - index => ${str[i]} -> ${str.codeUnitAt(i)}");
   }
 
+
+  if(index > 0 && index <= str.length)
+  {
+    String oldChar = str[index - 1];
+    int unicode = oldChar.codeUnitAt(0);
+
+    print("Index: $index");
+    print("Belgi: $oldChar");
+    print("Unicode: $unicode");
+  } else {
+    print("Xato! Index chegaradan tashqarida.");
+  }
 }
