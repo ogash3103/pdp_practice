679f376a83b1dba8c1bfcf0c217f74211dfa165e
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-15 14:35:24 +0500

    /b) Satr berilgan. //Satrdagi katta lotin harflari //sonini aniqlovchi funksiya tuzilsin.

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 void main() {
 // n butun soni berilgan (32 <= n <= 126).
 // Unikodi n ga teng bo'lgan belgini
@@ -10,7 +8,31 @@ void main() {
   if (n >= 32 || n <= 126) {
     String symbol = String.fromCharCode(n);
     print("Unicode: $n -> Belgi: $symbol");
-  }else{
+  } else {
     print("Xato! $n qiymat 32 va 126 orasida bo'lishi kerak.");
   }
+
+  //b) Satr berilgan.
+  //Satrdagi katta lotin harflari
+  //sonini aniqlovchi funksiya tuzilsin.
+  String text = "HellO";
+  int result = countUpperCase(text);
+
+  print("Katta harflar soni: $result");
+}
+
+//b) Satr berilgan.
+//Satrdagi katta lotin harflari
+//sonini aniqlovchi funksiya tuzilsin.
+
+int countUpperCase(String symbol) {
+  int count = 0;
+  for (int i = 0; i < symbol.length; i++) {
+    int code = symbol.codeUnitAt(i);
+    if (code >= 65 && code <= 90) {
+      count++;
+      print(symbol[i]);
+    }
+  }
+  return count;
 }
