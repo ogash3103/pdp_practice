a90a9cc94df218b0fde878601175974ebc80a6ce
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-15 15:30:53 +0500

    //a) Kiritilgan satrni //teskari tartibda o’giruvchi //return typeli funksiya tuzing tuzilsin.

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,54 +1,19 @@
 void main() {
-  //a) text1 va text2 satrlari berilgan.
-  //Agar text2 satri text1 satrida bo'lsa true,
-  //aks holda false chiqaruvchi dastur tuzing.
-  findTxt('Pdp Academy', 'hello');
+  // task a
+  print(replaceText('Flutter'));
 
-  //b) Str satrini tarkibida ‘A’ harfi necha
-  //marta borligini aniqlovchi dastur tuzing
-  findA('AAarfgAaA');
-
-  //c) text1 va text2 satrlari berilgan.
-  //text2 satrini text1 satrida takrorlanishlar
-  //sonini chigaruvchi dastur tuzing.
-
-  repeatText('Dart bu dasturlash tili va Dart da Flutter', 'dart');
+  // task b
 }
 
-void findTxt(String text1, String text2) {
-  bool result = text1.toLowerCase().contains(text2.toLowerCase());
-  print(result);
-}
+//a) Kiritilgan satrni
+//teskari tartibda o’giruvchi
+//return typeli funksiya tuzing tuzilsin.
 
-void findA(String str) {
-  int count = 0;
-
-  for (int i = 0; i < str.length; i++) {
-    if (str[i] == 'A') {
-      count++;
-      print('katta A => $count');
-    } else if (str[i] == 'a') {
-      count++;
-      print('kichik a => $count');
-    }
+String replaceText(String str) {
+  String reversedText = '';
+  for (int i = str.length - 1; i >= 0; i--) {
+    reversedText += str[i];
   }
-  print(count);
-}
 
-//c) text1 va text2 satrlari berilgan.
-//text2 satrini text1 satrida takrorlanishlar
-//sonini chigaruvchi dastur tuzing.
-
-void repeatText(String text1, String text2) {
-  int count = 0;
-  int startIndex = 0;
-
-  while (true) {
-    int index = text1.toLowerCase().indexOf(text2.toLowerCase(), startIndex);
-    if (index == -1) break;
-
-    count++;
-    startIndex = index + text2.length;
-  }
-  print("Takrorlanishlar soni: $count");
+  return reversedText;
 }
