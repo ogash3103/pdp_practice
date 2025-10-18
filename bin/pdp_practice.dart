9834d4b90b6812476e62003676fd715aac805559
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-18 12:31:45 +0500

    String ustida amallar

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,32 +1,27 @@
 void main() {
-  // task a
-  print(sameStarChar('xy*yzzz'));
+  //task a
+  print(nonStart('hello', 'there'));
 
   // task b
+  print(returnStr('PDP', 'Academy'));
+
+  // task c
+  print(findPdp('hello pdp'));
 }
 
-//a) Satr berilgan. Satrda har 1ta uchragan ‘*’
-//belgisidan 1ta oldingi va 1ta keyingi belgilar teng bo‘lsa,
-//true aks holatda false qaytaruvchi funksiya tuzing.
-//Agar‘*’dan 1ta oldin yoki 1ta keyin belgi bo'lmasa e'tiborga qarab.
-//sameStarChar("xy*yzz") → true
-//sameStarChar("xy*zzz") → false
-//sameStarChar("*xa*az") → true
-
-bool sameStarChar(String str) {
-  for (int i = 0; i < str.length; i++) {
-    if (str[i] == '*') {
-      if (i > 0 && i < str.length - 1) {
-        if (str[i - 1] != str[i + 1]) {
-          return false;
-        }
-      }
-    }
-  }
-  return true;
+// task a
+String nonStart(String a, String b) {
+  return a.substring(1) + b.substring(1);
 }
 
-//b) Satr berilgan. Ushbu satrni birinchi 2ta harfini qaytaruvchi funksiya tuzing. Agar satr uzunligi 2 dan kichik bolsa, osha satrni ozini qaytaring.
-//firstTwo("Salom") → “Sa"
-//firstTwo("abcdefg") → "ab"
-//firstTwo("ab") → "ab"
+// task b
+
+String returnStr(String a, String b) {
+  String tap = ' ';
+  return a + tap + b;
+}
+
+// task c
+int findPdp(String s) {
+  return s.indexOf('pdp');
+}
