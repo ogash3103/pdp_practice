1050769aec20f2a75bc75390e595381d29389e88
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-20 20:31:47 +0500

    n-chi element

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,36 +1,11 @@
 void main() {
-  // task a
-  print(leftPadWithDots('Hello', 6));
+  int n = 3;
 
-  // task b
+  List<String> list = ['hello', 'pdp', 'academy', 'you can learn anything'];
 
-  print(middleThree('Sarts'));
-
-  // task c
-
-  print(add('123', '34'));
-}
-
-// task a
-
-String leftPadWithDots(String s, int n) {
-  return s.length >= n ? s : s.padLeft(n, '.');
-}
-
-// task b
-
-String middleThree(String s) {
-  int mid = s.length ~/ 2;
-
-  return s.substring(mid - 1, mid + 2);
-}
-
-// task c
-
-String add(String str1, String str2) {
-  int sum1 = int.parse(str1);
-  int sum2 = int.parse(str2);
-  int sum = sum1 + sum2;
-
-  return sum.toString();
+  if (n < list.length) {
+    print("n-chi element: ${list[n]}");
+  } else {
+    print("Error!");
+  }
 }
