fcfc9c56505671219af2063c3283803444066ecf
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-21 18:41:55 +0500

    a) a, b sonlar va list berilgan. Shu listning a indexsiga b sonini joylashtiring va listni konsolga chiqaring.
    b) List va i soni berilgan. Shu listning i - chi indexsidagi elementni olib tashang.

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,24 +1,15 @@
 void main() {
-  final List<int> list = [12, 34, 2, 35, 6];
-  int maxNum = list[0];
-  int minNum = list[0];
-  int sum = 0;
-  int n = 5;
+  final List<int> list = [1, 2, 3, 4, 6];
+  int a = 4;
+  int b = 5;
+  int i = 3;
 
-  for (var item in list) {
-    if (item > maxNum) {
-      maxNum = item;
-    } else if (item < minNum) {
-      minNum = item;
-    }
-    sum += item;
+  if (a >= 0 && a < list.length) {
+    list[a] = b;
+    print(list);
+  } else {
+    print('xato');
   }
-
-  double average = sum / list.length;
-  list.insert(0, n);
-  
+  list.removeAt(i);
   print(list);
-  print("eng katta son: $maxNum");
-  print('eng kichik son: $minNum');
-  print('urta qiymat: $average');
 }
