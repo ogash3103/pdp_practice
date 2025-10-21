9354d20b1a3fa6f82e81e8ad010ba14d8291e1b9
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-21 18:53:10 +0500

    a) List berilgan. Shu listni teskarisini(elementlari teskari tartibda joylashgan) yangi listga ko'chirib o'tkazing va konsolga chiqaring.
    b) n va list berilgan. Shu listda nechta n soni borligini topib konsolga chiqaring.

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,15 +1,19 @@
 void main() {
-  final List<int> list = [1, 2, 3, 4, 6];
-  int a = 4;
-  int b = 5;
-  int i = 3;
-
-  if (a >= 0 && a < list.length) {
-    list[a] = b;
-    print(list);
-  } else {
-    print('xato');
-  }
-  list.removeAt(i);
+  final List<int> list = [7, 5, 2, 1, 3, 3, 6];
+  list.sort((a, b) => b.compareTo(a));
   print(list);
+
+  final List<int> reversedList = list.reversed.toList();
+  print(reversedList);
+
+  int n = 3;
+  int count = 0;
+
+  for (var item in list) {
+    if (item == n) {
+      count++;
+    }
+  }
+
+  print(count);
 }
