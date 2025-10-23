c6dc64d73a47e9cfdc0043085aacc92ba1e853fe
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2025-10-23 23:46:24 +0500

    practice

--- bin/pdp_practice.dart
+++ bin/pdp_practice.dart
@@ -1,11 +1,19 @@
 void main() {
-  final map = <String, String>{"b": "aaa", "c": "cake"};
+  final map = <String, String>{'a': 'dddd',"b": "aaaa", "c": "cake"};
 
-  if (map.containsKey('a')) {
-    map['b'] = map['a']!;
-  }else 
-  {
-    map['a'] = map['b']!;
+  if (map.containsKey('a') && map.containsKey('b')) {
+    final aValue = map['a']!;
+    final bValue = map['b']!;
+
+    if (aValue.length > bValue.length) {
+      map['c'] = aValue;
+    } else if (bValue.length > aValue.length) {
+      map['c'] = bValue;
+    } else {
+      map['a'] = '';
+      map['b'] = '';
+    }
   }
+
   print(map);
 }
